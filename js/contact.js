78b73ad54d7b2512d80aab205a62dca74a0b39aa
Abhisheak <abhisheak.sharma@blrsimplilearn.com>
2021-09-03 11:54:38 +0530

    First Commit

---
+++ js/contact.js
@@ -0,0 +1,7 @@
+function showAlert(){
+    document.getElementById('name').value='';
+    document.getElementById('number').value='';
+    document.getElementById('email').value='';
+    document.getElementById('message').value=''
+    alert('Your query has been recorded!!');
+}
